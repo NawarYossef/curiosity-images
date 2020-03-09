b7896733a2fcac7f836aaf420025507b1b095284
Nawar <nawaryossef2@gmail.com>
2020-03-08 22:09:43 -0400

    add loading spinner and outage page functionality

---
+++ src/components/OutagePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>We are experiencing an outage at the moment. Please try again later.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OutagePage',
+};
+</script>
+
+<style>
+</style>
