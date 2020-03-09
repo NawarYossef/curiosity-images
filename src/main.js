7ac2a736d6ca5b7c3dbbaf9dabdafc7ded23fade
Nawar <nawaryossef2@gmail.com>
2020-03-08 22:07:58 -0400

    add more config. Remove unused files

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from './App.vue';
+import Vue from 'vue';
+import router from './router';
+import store from './store';
+import vueDebounce from 'vue-debounce';
 
-Vue.config.productionTip = false
+Vue.use(vueDebounce, {
+  listenTo: 'input',
+});
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
